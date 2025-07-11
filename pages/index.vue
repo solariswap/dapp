17920c98799fdc914b7fd6e795a5b604acdabf1f
br0kenfiles <commandes.broken@gmail.com>
2025-07-11 21:37:35 +0200

    Update homepage headline text in `index.vue`

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@ import TextWordAppear from "~/components/base/animation/TextWordAppear.vue";
   <div class="flex flex-col flex-1 justify-center items-center">
     <TextWordAppear
       class="text-3xl laptop:text-5xl laptop:mb-10 mb-8 font-bold justify-center max-w-[28rem]"
-      text="Where DeFi moves at light speed"
+      text="Where DeFi moves at light speed EHEH"
     />
     <ClientOnly>
       <Swapper />
